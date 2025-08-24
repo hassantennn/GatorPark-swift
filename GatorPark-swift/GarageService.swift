09bb2b1028c96086e14bbd16474deeeb8e4294ff
Muhammad Hassan <158739616+hassantennn@users.noreply.github.com>
2025-08-24 21:21:10 +0100

    Add checkOut method to GarageService

--- GatorPark-swift/GarageService.swift
+++ GatorPark-swift/GarageService.swift
@@ -49,4 +49,33 @@ final class GarageService {
             }
         }
     }
+
+    func checkOut(from garage: Garage, completion: @escaping (Result<Void, Error>) -> Void) {
+        let ref = db.collection("garages").document(garage.id)
+
+        db.runTransaction({ (transaction, errorPointer) -> Any? in
+            let document: DocumentSnapshot
+            do {
+                document = try transaction.getDocument(ref)
+            } catch let error as NSError {
+                errorPointer?.pointee = error
+                return nil
+            }
+
+            guard let data = document.data(),
+                  let currentCount = data["currentCount"] as? Int else {
+                return nil
+            }
+
+            let newCount = max(currentCount - 1, 0)
+            transaction.updateData(["currentCount": newCount], forDocument: ref)
+            return nil
+        }) { (_, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        }
+    }
 }
