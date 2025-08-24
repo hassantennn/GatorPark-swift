b3f7f7f787f990698c8780cd3329ca87bcb04da1
Muhammad Hassan <158739616+hassantennn@users.noreply.github.com>
2025-08-24 21:09:06 +0100

    Revert "Replace REST API with Firestore"

--- GatorPark-swift/Garage.swift
+++ GatorPark-swift/Garage.swift
@@ -1,31 +1,14 @@
 import Foundation
 import CoreLocation
-import FirebaseFirestore
 
-struct Garage {
+struct Garage: Codable {
     let name: String
-    let location: GeoPoint
-    let capacity: Int
+    let latitude: Double
+    let longitude: Double
     var currentCount: Int
-    let isOpen: Bool
+    var capacity: Int
 
     var coordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
-    }
-
-    init?(from snapshot: DocumentSnapshot) {
-        guard let data = snapshot.data(),
-              let name = data["name"] as? String,
-              let location = data["location"] as? GeoPoint,
-              let capacity = data["capacity"] as? Int,
-              let currentCount = data["currentCount"] as? Int,
-              let isOpen = data["isOpen"] as? Bool else {
-            return nil
-        }
-        self.name = name
-        self.location = location
-        self.capacity = capacity
-        self.currentCount = currentCount
-        self.isOpen = isOpen
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
 }
--- GatorPark-swift/GarageService.swift
+++ GatorPark-swift/GarageService.swift
@@ -1,35 +1,70 @@
 import Foundation
-import FirebaseFirestore
 
 final class GarageService {
     static let shared = GarageService()
-    private let db = Firestore.firestore()
-    private var listener: ListenerRegistration?
+    private let baseURL = URL(string: "http://localhost:3000")!
+    private var webSocketTask: URLSessionWebSocketTask?
 
-    private init() {}
-
-    func observeGarages(onChange: @escaping ([Garage]) -> Void) {
-        listener?.remove()
-        listener = db.collection("garages").addSnapshotListener { snapshot, _ in
-            guard let documents = snapshot?.documents else {
-                onChange([])
-                return
+    func fetchGarages(completion: @escaping ([Garage]) -> Void) {
+        let url = baseURL.appendingPathComponent("garages")
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            if let data = data,
+               let garages = try? JSONDecoder().decode([Garage].self, from: data) {
+                DispatchQueue.main.async {
+                    completion(garages)
+                }
+            } else {
+                DispatchQueue.main.async { completion([]) }
             }
-            let garages = documents.compactMap { Garage(from: $0) }
-            onChange(garages)
+        }.resume()
+    }
+
+    func listenForUpdates(onUpdate: @escaping (Garage) -> Void) {
+        let wsURL = URL(string: "ws://localhost:3000")!
+        webSocketTask = URLSession.shared.webSocketTask(with: wsURL)
+        webSocketTask?.resume()
+        receiveUpdate(onUpdate)
+    }
+
+    private func receiveUpdate(_ onUpdate: @escaping (Garage) -> Void) {
+        webSocketTask?.receive { [weak self] result in
+            switch result {
+            case .success(let message):
+                switch message {
+                case .data(let data):
+                    if let garage = try? JSONDecoder().decode(Garage.self, from: data) {
+                        DispatchQueue.main.async { onUpdate(garage) }
+                    }
+                case .string(let string):
+                    if let data = string.data(using: .utf8),
+                       let garage = try? JSONDecoder().decode(Garage.self, from: data) {
+                        DispatchQueue.main.async { onUpdate(garage) }
+                    }
+                @unknown default:
+                    break
+                }
+            case .failure:
+                break
+            }
+            self?.receiveUpdate(onUpdate)
         }
     }
 
     func checkIn(garageName: String) {
-        db.collection("garages").document(garageName).updateData([
-            "currentCount": FieldValue.increment(Int64(1))
-        ])
+        updateGarage(garageName: garageName, action: "checkin")
     }
 
     func checkOut(garageName: String) {
-        db.collection("garages").document(garageName).updateData([
-            "currentCount": FieldValue.increment(Int64(-1))
-        ])
+        updateGarage(garageName: garageName, action: "checkout")
+    }
+
+    private func updateGarage(garageName: String, action: String) {
+        guard let encoded = garageName.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else { return }
+        let url = baseURL.appendingPathComponent("garages/")
+            .appendingPathComponent(encoded)
+            .appendingPathComponent(action)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        URLSession.shared.dataTask(with: request).resume()
     }
 }
-
