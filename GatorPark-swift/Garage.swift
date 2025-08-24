1a39d473a0f3841c8b3bb01b4032045100cc1a43
Muhammad Hassan <158739616+hassantennn@users.noreply.github.com>
2025-08-24 20:51:14 +0100

    Replace REST API with Firestore

--- GatorPark-swift/Garage.swift
+++ GatorPark-swift/Garage.swift
@@ -1,14 +1,31 @@
 import Foundation
 import CoreLocation
+import FirebaseFirestore
 
-struct Garage: Codable {
+struct Garage {
     let name: String
-    let latitude: Double
-    let longitude: Double
+    let location: GeoPoint
+    let capacity: Int
     var currentCount: Int
-    var capacity: Int
+    let isOpen: Bool
 
     var coordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+    }
+
+    init?(from snapshot: DocumentSnapshot) {
+        guard let data = snapshot.data(),
+              let name = data["name"] as? String,
+              let location = data["location"] as? GeoPoint,
+              let capacity = data["capacity"] as? Int,
+              let currentCount = data["currentCount"] as? Int,
+              let isOpen = data["isOpen"] as? Bool else {
+            return nil
+        }
+        self.name = name
+        self.location = location
+        self.capacity = capacity
+        self.currentCount = currentCount
+        self.isOpen = isOpen
     }
 }
